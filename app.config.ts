f5645d4fa1ebf780ab7107a6bba1b43299f980aa
Alexander Martin <alamowo@gmail.com>
2024-10-24 11:27:39 +0200

    first content update

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,8 @@
 export default defineAppConfig({
   // Configure Content Wind here
   cover: '/cover.jpg',
-  socials: {
-    twitter: 'Atinux',
-    github: 'Atinux/content-wind',
-    mastodon: '@atinux@webtoo.ls'
-  }
+  socials: {},
+  theme: {
+    dark: false
+  },
 })
